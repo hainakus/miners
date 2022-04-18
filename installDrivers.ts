4a3de689a5c834a5da2a1e0b6e4afcaaf9268a09
hainakus <djhainakosurge@gmail.com>
2022-04-19 00:21:37 +0100

    build installer

--- installDrivers.ts
+++ installDrivers.ts
@@ -3,7 +3,7 @@ export class InstallDrivers {
     constructor() {
 
         const exec = child_process.exec
-        const cmd = 'cd sh_commands && chmod +xinstall_nvidia.sh && ./install_nvidia.sh';
+        const cmd = 'cd sh_commands && chmod +x install_nvidia.sh && ./install_nvidia.sh';
 
         exec(cmd, function(error, stdout, stderr) {
             // command output is in stdout
