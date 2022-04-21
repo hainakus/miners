58ee56509ee347a9d6cdd0e4be36af761a113033
hainakus <djhainakosurge@gmail.com>
2022-04-21 01:04:32 +0100

    oc nvidia update

---
+++ sh_commands/oc_nvidia.sh
@@ -0,0 +1,46 @@
+#/bin/bash
+
+export DISPLAY=:0
+
+FANSPEED_1070TI=50
+FANSPEED_1080TI=50
+FANSPEED_3080TI_FTW=60
+
+# 1070 TI
+POWER_1070TI=120
+CLOCK_1070TI=100
+MEMORY_1070TI=1000
+
+# 1080 TI
+POWER_1080TI=140
+CLOCK_1080TI=100
+MEMORY_1080TI=850
+
+# 1080 TI FTW3 Edition
+POWER_1080TI_FTW=140
+CLOCK_1080TI_FTW=100
+MEMORY_1080TI_FTW=850
+
+# 1080 TI FTW3 Edition
+POWER_3080TI_FTW=315
+CLOCK_3080TI_FTW=1110
+MEMORY_3080TI_FTW=2000
+# Enable persistance
+nvidia-smi -pm 1
+
+# Adjust power limits
+nvidia-smi -i 0 -pl $POWER_3080TI_FTW
+
+
+# Adjust fan speeds
+nvidia-settings -a "[gpu:0]/GPUFanControlState=1" -a "[fan:0]/GPUTargetFanSpeed=$FANSPEED_3080TI"
+#nvidia-settings -a "[gpu:1]/GPUFanControlState=1" -a "[fan:1]/GPUTargetFanSpeed=$FANSPEED_1070TI"
+
+############## 1070 TI Memory & Clock Settings
+
+#GPU 0
+nvidia-settings -c :0 -a "[gpu:0]/GPUGraphicsClockOffset[3]=$CLOCK_3080TI"
+nvidia-settings -c :0 -a "[gpu:0]/GPUMemoryTransferRateOffset[3]=$MEMORY_3080TI"
+#GPU 1
+#nvidia-settings -c :0 -a "[gpu:1]/GPUGraphicsClockOffset[3]=$CLOCK_3080TI"
+#nvidia-settings -c :0 -a "[gpu:1]/GPUMemoryTransferRateOffset[3]=$MEMORY_3080TI"
