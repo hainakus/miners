d7f9465dc43c90a9c3b1ae40ef9cbec844525ccf
hainakus <djhainakosurge@gmail.com>
2022-04-21 02:22:51 +0100

    oc nvidia update

--- sh_commands/install_nvidia.sh
+++ sh_commands/install_nvidia.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo apt install nvidia-headless-510-server
 sudo apt install nvidia-utils-510-server
+sudo apt install x11-apps
+sudo apt install xterm
+sudo echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
