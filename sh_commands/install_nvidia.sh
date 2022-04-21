f8196f9dc18c394c147ddec53f0723213fca1b03
hainakus <djhainakosurge@gmail.com>
2022-04-21 02:17:24 +0100

    oc nvidia update

--- sh_commands/install_nvidia.sh
+++ sh_commands/install_nvidia.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt install nvidia-headless-510-server
-
+sudo apt install nvidia-utils-510-server
--- sh_commands/oc_nvidia.sh
+++ sh_commands/oc_nvidia.sh
@@ -25,6 +25,14 @@ MEMORY_1080TI_FTW=850
 POWER_3080TI_FTW=315
 CLOCK_3080TI_FTW=1110
 MEMORY_3080TI_FTW=2000
+
+# List all Cards
+nvidia-smi -L
+# DISABLE GUI BOOT
+sudo systemctl set-default multi-user
+
+nvidia-xconfig -a --allow-empty-initial-configuration --cool-bits=28 --use-display-device="DFP-0" --connected-monitor="DFP-0"
+
 # Enable persistance
 nvidia-smi -pm 1
 
@@ -33,14 +41,14 @@ nvidia-smi -i 0 -pl $POWER_3080TI_FTW
 
 
 # Adjust fan speeds
-nvidia-settings -a "[gpu:0]/GPUFanControlState=1" -a "[fan:0]/GPUTargetFanSpeed=$FANSPEED_3080TI"
-#nvidia-settings -a "[gpu:1]/GPUFanControlState=1" -a "[fan:1]/GPUTargetFanSpeed=$FANSPEED_1070TI"
+nvidia-settings -a "[gpu:0]/GPUFanControlState=1" -a "[fan:0]/GPUTargetFanSpeed=$FANSPEED_3080TI" --ctrl-display=:0
+#nvidia-settings -a "[gpu:1]/GPUFanControlState=1" -a "[fan:1]/GPUTargetFanSpeed=$FANSPEED_1070TI" --ctrl-display=:0
 
 ############## 1070 TI Memory & Clock Settings
 
 #GPU 0
-nvidia-settings -c :0 -a "[gpu:0]/GPUGraphicsClockOffset[3]=$CLOCK_3080TI"
-nvidia-settings -c :0 -a "[gpu:0]/GPUMemoryTransferRateOffset[3]=$MEMORY_3080TI"
+nvidia-settings -c :0 -a "[gpu:0]/GPUGraphicsClockOffset[3]=$CLOCK_3080TI" --ctrl-display=:0
+nvidia-settings -c :0 -a "[gpu:0]/GPUMemoryTransferRateOffset[3]=$MEMORY_3080TI" --ctrl-display=:0
 #GPU 1
 #nvidia-settings -c :0 -a "[gpu:1]/GPUGraphicsClockOffset[3]=$CLOCK_3080TI"
 #nvidia-settings -c :0 -a "[gpu:1]/GPUMemoryTransferRateOffset[3]=$MEMORY_3080TI"
